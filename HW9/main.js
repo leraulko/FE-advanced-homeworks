7a7a2e30787ca810ef1c5f7cef4a65df0d658515
leraulko <leraulko65@gmail.com>
2022-09-16 16:19:06 +0300

    chngs in 2 func

--- HW9/main.js
+++ HW9/main.js
@@ -16,11 +16,12 @@ function generateBlocksInterval() {
         const div = document.createElement('div');
         div.classList.add('square');
         div.style = 'background-color:' + "#" + ("00000" + Math.floor(Math.random() * Math.pow(16, 6)).toString(16)).slice(-6);
-        container.append(div);
 
         setInterval(() => {
             div.style = 'background-color:' + "#" + ("00000" + Math.floor(Math.random() * Math.pow(16, 6)).toString(16)).slice(-6);
         }, 1000);
+
+        container.append(div);
     }   
 }
 generateBlocksInterval()
