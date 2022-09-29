96702575c7609a03a93cec3bea8886a5e5c25e6e
leraulko <leraulko65@gmail.com>
2022-09-29 23:55:23 +0300

    added 11 homework

---
+++ HW11/main.js
@@ -0,0 +1,16 @@
+async function getRandomChinese(length) {
+    let result = '';
+
+    while (length) {
+        const sign = new Promise ((resolve) => {
+            setTimeout(() => resolve(String.fromCharCode(Date.now().toString().slice(-5))), 50);
+        });
+
+        result += await sign;
+        length--;
+    };
+
+    return result;
+};
+
+getRandomChinese(4).then(res => console.log(res));
